35613798c8fa0d3f1209ddf29f5f1712fb228516
Vladislav <drekmaro@gmail.com>
2018-08-11 14:03:20 +0300

    Add base request with url without method

--- pycryptoclients/request.py
+++ pycryptoclients/request.py
@@ -1,23 +1,30 @@
 from requests import Request
 
 
-__all__ = ('CCAPIRequest', 'DEFAULT_USER_AGENT')
+__all__ = ('CCAPIRequest', 'BaseCCRequest', 'DEFAULT_USER_AGENT')
 
 
 DEFAULT_USER_AGENT = 'pycryptoclients'
 
 
-class CCAPIRequest(Request):
+class BaseCCRequest(Request):
     api_method = None
-    is_private = False
     default_base_url = ''
 
     def __init__(self, base_url: str=None, **kwargs):
-        super(CCAPIRequest, self).__init__()
+        super(BaseCCRequest, self).__init__()
         self.base_url = base_url if base_url else self.default_base_url
         self.headers = {
             'Content-Type': 'application/json',
             'User-Agent': DEFAULT_USER_AGENT
         }
-        self.url = self.base_url.format(method=self.api_method)
         self.method = 'GET'
+
+
+class CCAPIRequest(BaseCCRequest):
+
+    is_private = False
+
+    def __init__(self, **kwargs):
+        super(CCAPIRequest, self).__init__(**kwargs)
+        self.url = self.base_url.format(method=self.api_method)
