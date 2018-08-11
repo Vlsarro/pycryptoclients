3e82e1123ff3033c3879b88d743fc012bc184a15
vlsarro <drekmaro@gmail.com>
2018-08-11 15:36:56 +0300

    Add test for getinfo method

---
+++ tests/test_wallets/__init__.py
@@ -0,0 +1,24 @@
+import json
+
+DASH_GETINFO_RESPONSE = json.dumps({
+    'result': {
+        'version': 1010000,
+        'protocolversion': 70210,
+        'walletversion': 62000,
+        'balance': 0.0,
+        'privatesend_balance': 0.0,
+        'blocks': 131248,
+        'timeoffset': 0,
+        'connections': 17,
+        'proxy': '',
+        'difficulty': 38.5859083016134,
+        'testnet': False,
+        'keypoololdest': 1533471684,
+        'keypoolsize': 999,
+        'paytxfee': 0.0,
+        'relayfee': 1e-05,
+        'errors': ''
+    },
+    'error': None,
+    'id': '1533990091718011'
+})
--- tests/test_wallets/test_dash.py
+++ tests/test_wallets/test_dash.py
@@ -1,10 +1,45 @@
 import unittest
+import requests_mock
 
+from pycryptoclients.request import DEFAULT_USER_AGENT
+from pycryptoclients.response import CCAPIResponse
 from pycryptoclients.wallets.dash.api import DashWalletRPCClient
+from tests.test_wallets import DASH_GETINFO_RESPONSE
+
+
+TEST_URL = 'http://127.0.0.1:7423/'
 
 
 class TestDashWalletRPCClient(unittest.TestCase):
 
     def setUp(self):
         super(TestDashWalletRPCClient, self).setUp()
-        self.wallet_client = DashWalletRPCClient()
+        self.client = DashWalletRPCClient(rpc_user='test_user', rpc_password='test_1', rpc_url=TEST_URL)
+
+    @requests_mock.Mocker()
+    def test_getinfo(self, m):
+        m.register_uri('POST', TEST_URL, text=DASH_GETINFO_RESPONSE)
+
+        resp = self.client.call('getinfo')
+
+        self.assertTrue(m.called)
+        self.assertEqual(m.call_count, 1)
+
+        req = m.request_history[0]
+        self.assertEqual(req.method, 'POST')
+        self.assertEqual(req.url, TEST_URL)
+
+        req_headers = req.headers
+        self.assertEqual(req_headers['User-Agent'], DEFAULT_USER_AGENT)
+        self.assertEqual(req_headers['Content-Type'], 'text/plain')
+
+        self.assertIsInstance(resp, CCAPIResponse)
+
+        data = resp.data
+        self.assertIsInstance(data, dict)
+        self.assertIsInstance(data['result'], dict)
+        self.assertIsNone(data['error'])
+
+
+if __name__ == '__main__':
+    unittest.main()
